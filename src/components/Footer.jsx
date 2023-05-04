ca3cc198efbcc098450ab30fc2c0b7d046751e4b
Anubhav <anubhavpal15june@gmail.com>
2023-05-04 23:14:42 +0530

    Footer Improvised

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,9 @@ import { logo } from '../assets'
 import { footerLinks, socialMedia } from '../constants'
 
 const Footer = () => (
-  <section className={`${styles.flexCenter} ${styles.paddingY}`}>
-    <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full flex-1`}>
-      <div className='flex flex-1 flex-col justify-start mr-10'>
+  <section className={`${styles.flexCenter} ${styles.padding}`}>
+    <div className={`${styles.flexStart} md:flex-row flex-1 flex-col mb-8 w-full gap-[200px]`}>
+      <div className='flex flex-col justify-start mr-10'>
         <img src={logo} alt="woobank" className='w-[266px] h-[72px] object-contain' />
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
       </div>
